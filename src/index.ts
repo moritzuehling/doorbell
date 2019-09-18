f47944bd7334c9ca377f5470eb09736adc203cd2
moritzuehling <github@moritzuehling.de>
2019-09-18 21:43:53 +0200

    logging

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { ReadStream } from "fs";
 const recorder = require('node-record-lpcm16');
-const fft = require('jsfft');
+
+console.log('starting the recording...');
 
 const stream = recorder.record({
   sampleRate: 16384,
@@ -12,7 +13,9 @@ const short = Math.pow(2, 15);
 const invShort = 1 / short;
 
 const n = Date.now();
+console.log('starting recording loop...');
 stream.on('data', (rawData: Buffer) => {
+  console.log('new data');
   let minValue = 1;
   let maxValue = -1;
 
